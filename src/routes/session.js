82060fe9765e75b2a71c15f32481154a1087e7cb
Yudi <singh.yudi10@gmail.com>
2021-03-11 16:16:32 +0530

    bug fix

--- src/routes/session.js
+++ src/routes/session.js
@@ -11,15 +11,15 @@ sessionRouter.post("", async (req, res) => {
   try {
     const { email, password } = req.body;
     await Joi.validate({ email, password }, signIn);
-
+    console.log(email, password);
     const user = await User.findOne({ email });
     if (user && user.comparePasswords(password)) {
       const sessionUser = sessionizeUser(user);
 
-      req.session.user = sessionUser
+      req.session.user = sessionUser;
       res.send(sessionUser);
     } else {
-      throw new Error('Invalid login credentials');
+      throw new Error("Invalid login credentials");
     }
   } catch (err) {
     res.status(401).send(parseError(err));
@@ -30,20 +30,20 @@ sessionRouter.delete("", ({ session }, res) => {
   try {
     const user = session.user;
     if (user) {
-      session.destroy(err => {
-        if (err) throw (err);
+      session.destroy((err) => {
+        if (err) throw err;
         res.clearCookie(SESS_NAME);
         res.send(user);
       });
     } else {
-      throw new Error('Something went wrong');
+      throw new Error("Something went wrong");
     }
   } catch (err) {
     res.status(422).send(parseError(err));
   }
 });
 
-sessionRouter.get("", ({ session: { user }}, res) => {
+sessionRouter.get("", ({ session: { user } }, res) => {
   res.send({ user });
 });
 
